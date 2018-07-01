1c64e1976fb8a48d844367bc66d5c1c129f3eb68
selimelawwa <selimalawwa335@gmail.com>
2018-07-01 03:57:07 +0200

    2nd

--- app/controllers/beacons_controller.rb
+++ app/controllers/beacons_controller.rb
@@ -1,4 +1,5 @@
 class BeaconsController < ApplicationController
+    before_action :set_beacon, only: [:show]
     def index
     @beacons = Beacon.all
     render json: {status: 'SUCCESS', message: 'Loaded all beacons', data: @beacons}, status: :ok
@@ -6,19 +7,22 @@ class BeaconsController < ApplicationController
     end
 
     def show
-        
-        @beacon = Beacon.where("major = ? AND minor = ?",params[:major],params[:minor]) 
         @campaign = Campaign.find_by_beacon_id(@beacon)
         if @campaign.nil?
-            render json: {errors: 'Not found'}, status: 404
+            render json: {errors: 'Not found', status: 404}
         else
             if @campaign.enabled? && @campaign.start_time < DateTime.now() && @campaign.end_time > DateTime.now()
-                render json: { message: 'Loaded the campaign', data: @campaign}, status: :ok
+                render json: { message: 'Loaded the campaign', data: @campaign, status: :ok}
             else
-                render json: { message: 'Campaign has ended'}, status: :ok
+                render json: { message: 'Campaign has ended', status: :ok}
             end
         end
 
+    end
+
+    private
+    def set_beacon
+        @beacon = Beacon.where("major = ? AND minor = ?",params[:major],params[:minor])     
     end
 
 end
